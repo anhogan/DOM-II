12289b431aa5f2be1704be09421a80c40e5b2be8
Amanda Hogan <ahogan@teamworks.com>
2020-01-13 17:27:47 -0500

    Event selectors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
-// Your code goes here
+// Event Handlers
+// `mouseover` // Let's Go text -> Arrow
+const letsGo = document.querySelector('.text-content h2');
+
+// `keydown` // Body background turns black
+const body = document.querySelector('body');
+
+// `copy` // Excursion description -> alert (copied to clipboard)
+const excursion = document.querySelectorAll('.destination');
+
+// `drag / drop` // Drag: Middle Image, border appears...Drop: Middle Image, bottom image disappears (Stop border propogation?)
+const dragImg = document.querySelector('.img-fluid');
+
+// `load` // Sign me up button -> grays out
+const signUp = document.querySelectorAll('.btn');
+
+// `focus` // Welcome to the Fun Bus text -> Animation
+const welcome = document.querySelector('h2');
+
+// `resize` // Change page size -> alert (that isn't fun)
+const pageSize = document.querySelector('html');
+
+// `scroll` // Nav background turns yellow
+const navBg = document.querySelector('header');
+
+// `select` // Change text background to yellow
+const paragraphText = document.querySelectorAll('p');
+
+// `dblclick` // Fun Bus in Nav bar -> Bus Image
+const funBus = document.querySelector('.logo-heading');
+
+// `wheel` // Main Image zoom
+const busImg = document.querySelector('img');
+
+// Nav Refresh
