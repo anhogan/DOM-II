2187ec75d2ad1529b1fea74809cf8204edf3dec0
Amanda Hogan <ahogan@teamworks.com>
2020-01-13 18:31:50 -0500

    Event Listeners

--- js/index.js
+++ js/index.js
@@ -1,35 +1,71 @@
 // Event Handlers
-// `mouseover` // Let's Go text -> Arrow
-const letsGo = document.querySelector('.text-content h2');
-
-// `keydown` // Body background turns black
 const body = document.querySelector('body');
+body.addEventListener('keydown', (event) => {
+  body.style.backgroundColor = "black";
+  body.style.color = "yellow";
+});
+body.addEventListener('keyup', (event) => {
+  body.style.backgroundColor = "white";
+  body.style.color = "black";
+});
 
-// `copy` // Excursion description -> alert (copied to clipboard)
-const excursion = document.querySelectorAll('.destination');
+// const navBg = document.querySelector('header');
+// navBg.addEventListener('scroll', (event) => {
+//   navBg.style.backgroundColor = "yellow";
+// });
 
-// `drag / drop` // Drag: Middle Image, border appears...Drop: Middle Image, bottom image disappears (Stop border propogation?)
-const dragImg = document.querySelector('.img-fluid');
-
-// `load` // Sign me up button -> grays out
-const signUp = document.querySelectorAll('.btn');
-
-// `focus` // Welcome to the Fun Bus text -> Animation
-const welcome = document.querySelector('h2');
-
-// `resize` // Change page size -> alert (that isn't fun)
-const pageSize = document.querySelector('html');
-
-// `scroll` // Nav background turns yellow
-const navBg = document.querySelector('header');
-
-// `select` // Change text background to yellow
-const paragraphText = document.querySelectorAll('p');
-
-// `dblclick` // Fun Bus in Nav bar -> Bus Image
 const funBus = document.querySelector('.logo-heading');
+funBus.addEventListener('dblclick', (event) => {
+  funBus.textContent = "Hop On!";
+});
 
-// `wheel` // Main Image zoom
 const busImg = document.querySelector('img');
+busImg.addEventListener('wheel', (event) => {
+  busImg.src = "https://images.unsplash.com/photo-1547010932-0a13d4508fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+});
+// busImg.addEventListener('resize', (event) => {
+//   alert("Resizing isn't fun!")
+// });
+
+const welcome = document.querySelector('h2');
+welcome.addEventListener('mouseleave', (event) => {
+  welcome.style.fontFamily = 'Roboto';
+  welcome.style.color = "goldenrod";
+});
+
+// FIX THIS!!! Select?
+const paragraphText = document.querySelector('p');
+paragraphText.addEventListener('select', (event) => {
+  paragraphText.style.background = "yellow";
+});
+
+const letsGo = document.querySelector('.text-content h2');
+letsGo.addEventListener('mouseover', (event) => {
+  letsGo.textContent = "=====>>>";
+  letsGo.style.color = "red";
+});
+letsGo.addEventListener('mouseleave', (event) => {
+  letsGo.textContent = "Let's Go";
+  letsGo.style.color = "black";
+});
+
+const dragImg = document.querySelector('.img-fluid');
+dragImg.addEventListener('drag', (event) => {
+  dragImg.style.border = "2px solid yellow";
+});
+
+const excursion = document.querySelector('.destination');
+excursion.addEventListener('copy', (event) => {
+  alert('Copied to clipboard');
+});
+
+const signUp = document.querySelector('.btn');
+signUp.addEventListener('contextmenu', (event) => {
+  signUp.style.backgroundColor = "darkgray";
+  signUp.style.color = "lightgray";
+});
+signUp.addEventListener('click', (event) => {
+  alert("You're signed up! Pack those bags!")
+});
 
 // Nav Refresh
