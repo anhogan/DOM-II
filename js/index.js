b01c8886ad8a069800d9601173c53c2e3cc6c5da
Amanda Hogan <ahogan@teamworks.com>
2020-01-14 14:12:51 -0500

    Styling changes and listener additions

--- js/index.js
+++ js/index.js
@@ -1,45 +1,48 @@
 // Event Handlers
 const body = document.querySelector('body');
-body.addEventListener('keydown', (event) => {
+body.addEventListener('keydown', () => {
   body.style.backgroundColor = "black";
   body.style.color = "yellow";
 });
-body.addEventListener('keyup', (event) => {
+body.addEventListener('keyup', () => {
   body.style.backgroundColor = "white";
   body.style.color = "black";
 });
+window.addEventListener('resize', () => {
+  body.style.backgroundColor = "yellow";
+})
 
 const funBus = document.querySelector('.logo-heading');
-funBus.addEventListener('dblclick', (event) => {
+funBus.addEventListener('dblclick', () => {
   funBus.textContent = "Hop On!";
 });
 
 const busImg = document.querySelector('img');
-busImg.addEventListener('wheel', (event) => {
+busImg.addEventListener('wheel', () => {
   busImg.src = "https://images.unsplash.com/photo-1547010932-0a13d4508fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
 });
 
 const welcome = document.querySelector('h2');
-welcome.addEventListener('mouseleave', (event) => {
+welcome.addEventListener('mouseleave', () => {
   welcome.style.fontFamily = 'Roboto';
   welcome.style.color = "goldenrod";
 });
 
 const letsGo = document.querySelector('.text-content h2');
-letsGo.addEventListener('mouseover', (event) => {
+letsGo.addEventListener('mouseover', () => {
   letsGo.textContent = "=====>>>";
   letsGo.style.color = "red";
 });
-letsGo.addEventListener('mouseleave', (event) => {
+letsGo.addEventListener('mouseleave', () => {
   letsGo.textContent = "Let's Go";
   letsGo.style.color = "black";
 });
 
 const dragImg = document.querySelector('.img-fluid');
-dragImg.addEventListener('drag', (event) => {
-  dragImg.style.border = "2px solid yellow";
+dragImg.addEventListener('drag', () => {
+  dragImg.style.border = "2px solid goldenrod";
 });
-dragImg.addEventListener('contextmenu', (event) => {
+dragImg.addEventListener('contextmenu', () => {
   alert('Visit this destination on our Island Getaway excursion. Sign up now to reserve your spot!');
 });
 
@@ -52,10 +55,10 @@ signUp.addEventListener('click', (event) => {
 });
 
 const excursion = document.querySelector('.destination');
-excursion.addEventListener('copy', (event) => {
+excursion.addEventListener('copy', () => {
   excursion.style.color = 'red';
 });
-excursion.addEventListener('click', (event) => {
+excursion.addEventListener('click', () => {
   alert('More details on the selected trip...')
 });
 
@@ -64,3 +67,6 @@ const navRefresh = document.querySelector('nav');
 navRefresh.addEventListener('click', (event) => {
   event.preventDefault();
 });
+window.addEventListener('scroll', () => {
+  navRefresh.style.backgroundColor = "yellow";
+})
